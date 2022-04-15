aa6d7f80fed36276e35b5505e224ce35e62f3c0d
mrcxmrj <marcin.zurawel@gmail.com>
2022-04-15 21:12:30 +0200

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    Container,
+} from "@chakra-ui/react";
+
+function App() {
+    return <Container>asdf</Container>;
+}
+
+export default App;
